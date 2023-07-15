9b3e8b188e59f8083e8fc74866afcdac119a01a0
Hussien <hussien.fahmy98@gmail.com>
2023-07-15 19:23:31 +0300

    Modified UiEventHandler.kt to hide keyboard on show snack bar

--- core-ui/src/main/java/com/hussienFahmy/core_ui/presentation/util/UiEventHandler.kt
+++ core-ui/src/main/java/com/hussienFahmy/core_ui/presentation/util/UiEventHandler.kt
@@ -4,27 +4,30 @@ import android.widget.Toast
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import com.hussienFahmy.core_ui.presentation.model.UiEvent
 import kotlinx.coroutines.flow.Flow
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun UiEventHandler(
     key: Any? = Unit,
     uiEvent: Flow<UiEvent>,
     snackBarHostState: SnackbarHostState? = null,
-    onShowSnackBar: () -> Unit = {},
 ) {
     val context = LocalContext.current
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     LaunchedEffect(key) {
         uiEvent.collect { event ->
             when (event) {
                 is UiEvent.ShowSnackBar -> {
+                    keyboardController?.hide()
                     snackBarHostState?.showSnackbar(
                         message = event.message.asString(context = context)
                     )
-                    onShowSnackBar()
                 }
 
                 is UiEvent.ShowToast -> {
