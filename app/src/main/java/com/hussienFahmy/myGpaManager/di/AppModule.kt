014dd748912eebd31ab4f7a15c84c90eec985f43
Hussien Fahmy <contact@h-fahmy.com>
2025-09-12 12:02:08 +0300

    Refactor AppModule to use viewModelOf
    This commit updates the Koin dependency injection setup in `AppModule.kt` to use the `viewModelOf` function for providing `MainViewModel` and `MoreViewModel`. This is a more concise way to declare ViewModels with Koin.

--- app/src/main/java/com/hussienFahmy/myGpaManager/di/AppModule.kt
+++ app/src/main/java/com/hussienFahmy/myGpaManager/di/AppModule.kt
@@ -3,11 +3,11 @@ package com.hussienfahmy.myGpaManager.di
 import androidx.work.WorkManager
 import com.hussienfahmy.myGpaManager.MainViewModel
 import com.hussienfahmy.myGpaManager.navigation.screens.more.MoreViewModel
-import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appKoinModule = module {
     single { WorkManager.getInstance(get()) }
-    viewModel { MainViewModel(get(), get()) }
-    viewModel { MoreViewModel(get(), get()) }
+    viewModelOf(::MainViewModel)
+    viewModelOf(::MoreViewModel)
 }
