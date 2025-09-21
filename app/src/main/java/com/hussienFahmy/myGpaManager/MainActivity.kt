e092e2fd771a5ab3a12a924e9c73ea20bf801673
Hussien Fahmy <contact@h-fahmy.com>
2025-09-21 14:20:56 +0300

    Enable edge-to-edge display in MainActivity
    This commit calls `enableEdgeToEdge()` in the `onCreate` method of `MainActivity` to allow the app to draw behind the system bars for a more immersive experience.

--- app/src/main/java/com/hussienFahmy/myGpaManager/MainActivity.kt
+++ app/src/main/java/com/hussienFahmy/myGpaManager/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hussienfahmy.myGpaManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -54,6 +55,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         // Request notification permission
         if (!PermissionHelper.hasNotificationPermission(this)) {
